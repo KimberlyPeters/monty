4f8de541025360e7ebaa9c5d6f6f0080244d3b0b
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-05-25 07:17:44 -0700

    pint

--- pint.c
+++ pint.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
- * f_pint - prints the top
- * @head: stack head
+ * f_pint - prints top
+ * @head: head
  * @counter: line_number
- * Return: no return
+ * Return: NO
 */
 void f_pint(stack_t **head, unsigned int counter)
 {
