13bf0da32d6606b065783a54ba05effebbeac78a
hertheyhermee <tiamiyuhayzed@gmail.com>
2023-05-25 07:40:48 -0700

    nop

--- nop.c
+++ nop.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
-  *f_nop- nothing
-  *@head: stack head
-  *@counter: line_number
-  *Return: no return
+  * f_nop - nothing
+  * @head: head
+  * @counter: line_number
+  * Return: no
  */
 void f_nop(stack_t **head, unsigned int counter)
 {
